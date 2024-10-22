0e7cadd7e049304c8710473338579eff91bee9ce
SamarV-121 <samarvispute121@pm.me>
2024-10-22 14:17:24 +0530

    blossom: Compile vndk blobs directly from prebuilts/vndk dir
    Change-Id: I821698e203e6457aecbd59a219a0ba5c5c1413e7
    Signed-off-by: fukiame <fukiame@proton.me>

--- device.mk
+++ device.mk
@@ -213,6 +213,11 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/props/vendor,$(TARGET_COPY_OUT_VENDOR)/etc/properties)
 
 # VNDK
+PRODUCT_PACKAGES += \
+    libutils-v32 \
+    libutils-v30 \
+    libui-v32
+
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
