a39e08f2794dc7efab44e85cd9ff6cf84f5cb6b0
Yurical <yurical1@outlook.com>
2024-12-22 18:59:21 +0530

    blossom: Do not include other java mini debug info
    In the same manner of PRODUCT_SYSTEM_SERVER_DEBUG_INFO.
    We explicitly add PRODUCT_OTHER_JAVA_DEBUG_INFO here since
    WITH_DEXPREOPT_DEBUG_INFO can be overriden with
    PRODUCT_SYSTEM_SERVER_DEBUG_INFO and PRODUCT_OTHER_JAVA_DEBUG_INFO.
    
    Change-Id: Ib9173bdbeaf3e279256be10c4082036c486dd154
    Signed-off-by: Yurical <yurical1@outlook.com>
    Signed-off-by: fukiame <fukiame@proton.me>

--- device.mk
+++ device.mk
@@ -39,6 +39,7 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
 # Reduce system server verbosity.
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+PRODUCT_OTHER_JAVA_DEBUG_INFO := false
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
