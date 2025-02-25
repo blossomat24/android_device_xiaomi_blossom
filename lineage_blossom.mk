e59c27d205b5c07895ad5e6fb6042b1987d0e1cc
fukiame <fukiame@proton.me>
2025-02-25 18:03:41 +0700

    blossom: axiom bringup

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -21,6 +21,18 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # include private signing keys
 -include vendor/niigo-priv/keys/keys.mk
 
+# AxionAOSP flags start
+# Camera
+AXION_CAMERA_REAR_INFO := 13
+AXION_CAMERA_FRONT_INFO := 5
+
+# Processor
+AXION_PROCESSOR := Mediatek_Helio_G25
+
+# Maintainer
+AXION_MAINTAINER := fukiame
+# AxionAOSP flags end
+
 PRODUCT_NAME := lineage_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
