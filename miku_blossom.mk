30f2a2294d754128a9405e6f46384b0942b2776f
fukiame <fukiame@proton.me>
2025-02-27 21:02:24 +0700

    blossom: never compile audiofx
    complements b5eee6c0 "blossom: remove_packages: add an option to forcefully nuke AudioFX"
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- miku_blossom.mk
+++ miku_blossom.mk
@@ -18,6 +18,9 @@ $(call inherit-product, vendor/miku/build/product/miku_product.mk)
 # include private signing keys
 -include vendor/niigo-priv/keys/keys.mk
 
+# always nuke audiofx
+NUKE_AUDIOFX := true
+
 # Maintainer
 MIKU_MASTER := fukiame
 
