c6bb1eaa8e6a9b2ee780c8ab6037ae7338a5bb16
fukiame <fukiame@proton.me>
2025-02-28 23:19:42 +0700

    blossom: ship FOSS Browser
    lack of default browset gotta kill
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- miku_blossom.mk
+++ miku_blossom.mk
@@ -18,6 +18,9 @@ $(call inherit-product, vendor/miku/build/product/miku_product.mk)
 # include private signing keys
 -include vendor/niigo-priv/keys/keys.mk
 
+# local patch - ship FOSS Browser
+TARGET_SHIP_FOSSBROWSER := true
+
 # always nuke audiofx
 NUKE_AUDIOFX := true
 
