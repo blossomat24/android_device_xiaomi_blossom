253b58498041a12e6933a1b69d0ee6a6b1ff0939
fukiame <fukiame@proton.me>
2025-02-28 23:04:03 +0700

    blossom: init: decommonize lmkd props
    more precise tunings for less kills
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- init/init_blossom.cpp
+++ init/init_blossom.cpp
@@ -57,6 +57,10 @@ void vendor_load_properties()
     string heapstartsize, heapgrowthlimit, heapsize, heapminfree,
 			heapmaxfree, heaptargetutilization;
 
+    // lmkd configuration
+    string partialstall, completestall, thrashlim, thrashlimdec,
+			swapfreelow, upressure;
+
     struct sysinfo sys;
     sysinfo(&sys);
 
@@ -68,6 +72,14 @@ void vendor_load_properties()
         heaptargetutilization = "0.5";
         heapminfree = "8m";
         heapmaxfree = "32m";
+        // from lmkd defaults for high perf devices
+        // except completestall, default 700
+        partialstall = "70";
+        completestall = "160";
+        thrashlim = "100";
+        thrashlimdec = "10";
+        swapfreelow = "20";
+        upressure = "50";
     } else if (sys.totalram > 3072ull * 1024 * 1024) {
         // from - phone-xxhdpi-4096-dalvik-heap.mk
         heapstartsize = "8m";
@@ -76,6 +88,14 @@ void vendor_load_properties()
         heaptargetutilization = "0.6";
         heapminfree = "8m";
         heapmaxfree = "16m";
+        // from lmkd defaults for high perf devices
+        // tuned lower, clamped stall
+        partialstall = "80";
+        completestall = "180";
+        thrashlim = "70";
+        thrashlimdec = "20";
+        swapfreelow = "18";
+        upressure = "60";
         property_override("ro.config.art_lowmem", "true");
     } else {
         // from - phone-xhdpi-2048-dalvik-heap.mk
@@ -86,6 +106,14 @@ void vendor_load_properties()
         heaptargetutilization = "0.75";
         heapminfree = "512k";
         heapmaxfree = "8m";
+        // from lmkd defaults for low ram devices
+        // tuned a bit "higher end"
+        partialstall = "120";
+        completestall = "360";
+        thrashlim = "60";
+        thrashlimdec = "35";
+        swapfreelow = "15";
+        upressure = "70";
         property_override("ro.config.art_lowmem", "true");
     }
 
@@ -95,4 +123,11 @@ void vendor_load_properties()
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+
+    property_override("ro.lmk.psi_partial_stall_ms", partialstall);
+    property_override("ro.lmk.psi_complete_stall_ms", completestall);
+    property_override("ro.lmk.thrashing_limit", thrashlim);
+    property_override("ro.lmk.thrashing_limit_decay", thrashlimdec);
+    property_override("ro.lmk.swap_free_low_percentage", swapfreelow);
+    property_override("ro.lmk.upgrade_pressure", upressure);
 }
