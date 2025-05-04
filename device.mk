ea7335784b25ec647dc2ed5f1f6ad50ac7b7887c
alternoegraha <noegrahachan@gmail.com>
2025-05-04 10:41:04 +0700

    blossom: Move `TARGET_SCREEN_DENSITY` to device.mk
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>
    Signed-off-by: fukiame <fukiame@proton.me>

--- device.mk
+++ device.mk
@@ -147,6 +147,8 @@ PRODUCT_PACKAGES += \
     android.hardware.light-service.blossom
 
 # Display
+TARGET_SCREEN_DENSITY := 320
+
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
     android.hardware.graphics.composer@2.1.vendor \
