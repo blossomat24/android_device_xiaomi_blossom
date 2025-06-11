1211ffd1365cf59dde3bc822aa0b94ec2798014b
Giovanni Ricca <giovanniricca@proton.me>
2025-06-11 09:46:01 +0530

    blossom: Enable ELF checks for libimsma
    Rename libsink library to not conflict with the rust module
    
    Change-Id: I16028606d42d63525b5ef3130d86279bf35e26e1
    Signed-off-by: fukiame <fukiame@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -75,6 +75,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libprocessgroup_shim.so" "${2}" || "${PATCHELF}" --add-needed "libprocessgroup_shim.so" "${2}"
             ;;
+        system_ext/lib/libimsma.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libsink.so" "libsink-mtk.so" "${2}"
+            ;;
         *)
             return 1
             ;;
