8c8329a2da95c69e41f1c4152ed28592e215b59f
fukiame <fukiame@proton.me>
2025-08-18 12:04:33 +0700

    Revert "blossom: ship FOSS Browser"
    This reverts commit c6bb1eaa8e6a9b2ee780c8ab6037ae7338a5bb16.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- miku_blossom.mk
+++ miku_blossom.mk
@@ -21,9 +21,6 @@ $(call inherit-product, vendor/miku/build/product/miku_product.mk)
 # include private signing keys
 -include vendor/niigo-priv/keys/keys.mk
 
-# local patch - ship FOSS Browser
-TARGET_SHIP_FOSSBROWSER := true
-
 # always nuke audiofx
 NUKE_AUDIOFX := true
 
