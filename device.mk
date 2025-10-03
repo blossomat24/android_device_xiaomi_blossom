fc64b4ead25c29c62e9a250e130c6c3453ab5171
fukiame <fukiame@proton.me>
2025-10-03 10:37:52 +0700

    fixup! blossom: rootdir: actually bundle volte rc
    Signed-off-by: fukiame <fukiame@proton.me>

--- device.mk
+++ device.mk
@@ -90,7 +90,10 @@ PRODUCT_PACKAGES += \
     init.project.rc \
     init.sensor_1_0.rc \
     ueventd.mtk.rc \
-    init.mt6765.volte.rc \
+    init.angelica.volte.rc \
+    init.angelicain.volte.rc \
+    init.angelican.volte.rc \
+    init.cattail.volte.rc \
     init.target.rc
 
 # Recovery
