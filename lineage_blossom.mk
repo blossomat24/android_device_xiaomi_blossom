2531618e0e5920ee5b7dd5304b241fc320fae89b
zyexro <zyexro@proton.me>
2026-02-17 15:57:47 +0100

    Revert "blossom: lmodroid buildbot"
    This reverts commit 4fe5193c092a1a099d191361b320472da8afacae.

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -13,12 +13,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Include private signing keys
 -include vendor/extra/product.mk
 
-PRODUCT_NAME := lmodroid_blossom
+PRODUCT_NAME := lineage_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
