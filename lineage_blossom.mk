f84a66a60a8ed9dce110a88fd03a8c4e08e9025d
zyexro <zyexro@proton.me>
2026-02-23 08:09:43 +0100

    blossom: gapps included by default
    * from: https://gitlab.com/MindTheGapps/vendor_gapps
    
    Signed-off-by: zyexro <zyexro@proton.me>

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -15,6 +15,9 @@ $(call inherit-product, device/xiaomi/blossom/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit mindthegapps stuff.
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+
 # Include private signing keys
 -include vendor/extra/product.mk
 
